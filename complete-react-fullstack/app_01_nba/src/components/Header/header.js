ce0e29ca6fd465a0d5df34695491413b6711dcac
Richelly Italo <richellyitalo@gmail.com>
2018-05-01 21:30:52 -0300

    CompleteReactFullsack: app_01_nba ejected

---
+++ complete-react-fullstack/app_01_nba/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './header.css'
+
+const Header = () => (
+  <header className={style.header}>
+    <div>
+      Topo
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ complete-react-fullstack/app_01_nba/src/routes.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home/home'
+import Layout from './hoc/Layout/layout'
+
+class Routes extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
+    )
+  }
+}
+
+export default Routes
