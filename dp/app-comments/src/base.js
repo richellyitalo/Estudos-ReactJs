4042ee8e819db42d127d444a5e364d4f66b9196d
richellyitalo <richellyitalo@gmail.com>
2018-07-25 00:51:40 -0300

    Omitido dados da key

--- dp/app-comments/src/base.js
+++ dp/app-comments/src/base.js
@@ -2,12 +2,12 @@ import Rebase from 're-base'
 import Firebase from 'firebase'
 
 var app = Firebase.initializeApp({
-  apiKey: "AIzaSyBdVSYmS1WBqB9H_vMygdtxdqEWku6Gf6k",
-  authDomain: "reactjs-1a485.firebaseapp.com",
-  databaseURL: "https://reactjs-1a485.firebaseio.com",
-  projectId: "reactjs-1a485",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "933219977375"
+  messagingSenderId: ""
 })
 
 var base = Rebase.createClass(app.database())
