f1d67bbcd80546ade7f7e86bba9d2b6f13e38485
Richelly Italo <richellyitalo@gmail.com>
2018-09-23 10:00:26 -0300

    MERN: Reactjs client iniciado.

---
+++ front-to-back-mern/client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Components
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
--- front-to-back-mern/server.js
+++ front-to-back-mern/server.js
@@ -37,6 +37,6 @@ app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server na porta ${port}`));
