42497c554e2b6e1fb1d06f6becea4f57696b93df
Richelly Italo <richellyitalo@gmail.com>
2018-10-05 01:56:10 -0300

    MERN: Pronto para deploy

--- front-to-back-mern/server.js
+++ front-to-back-mern/server.js
@@ -2,17 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path')
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongo;
+
+// Opções de conexão
+let options = {}
+if (db.user !== '') {
+  options = { auth: {
+      user: db.user,
+      password: db.password
+    },
+    dbName: db.dbName
+  }
+}
 
 // Connection MongoDB
 mongoose
-  .connect(db)
+  .connect(db.uri, options)
   .then(() => console.log('Conectado do MongoDB...'))
   .catch(err =>
     console.log('Não foi possível conectar ao MongoDB', err.message)
@@ -36,6 +48,16 @@ app.get('/', (req, res) => res.send('App MERN'));
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
